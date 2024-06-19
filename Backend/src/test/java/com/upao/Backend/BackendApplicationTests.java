b372e4facf94223348e5d8fb8ff608afc5a7e80e
Winan03 <Jnacarinoa1@upao.edu.pe>
2024-06-19 00:43:00 -0500

    Añadiendo proyecto backend y frontend

---
+++ Backend/src/test/java/com/upao/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upao.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
