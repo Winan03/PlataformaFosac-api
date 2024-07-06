802abef673aa9a6e70fc5bad1ffa52558b74090d
Winan03 <Jnacarinoa1@upao.edu.pe>
2024-07-06 09:36:26 -0500

    Implementación del Servicio de Autenticación (AuthService)

---
+++ backend/servicio_authenticacion/src/main/java/com/mecanicafosac/servicio_authenticacion/ServicioAuthenticacionApplication.java
@@ -0,0 +1,13 @@
+package com.mecanicafosac.servicio_authenticacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioAuthenticacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioAuthenticacionApplication.class, args);
+	}
+
+}
---
+++ backend/servicio_authenticacion/src/test/java/com/mecanicafosac/servicio_authenticacion/ServicioAuthenticacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mecanicafosac.servicio_authenticacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioAuthenticacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
